91349c82792573d49ddb841675de39aa15463a48
TakHyunJoon <qlroxn@naver.com>
2017-11-10 06:26:35 -0800

    1110tak

--- lib/manual.c
+++ lib/manual.c
@@ -2,10 +2,12 @@
 
 void manual()
 {
-	printf("---------------------------------------\n");
+	printf("--------------------------------------------\n");
 	printf("There are 4 types of calculator\n");
 	printf("Please enter the number of the menu you want\n");
-	printf("1. Default calculator\n2.Enginerring calculator\n3.Programmer calculator\n4.Date calculator\n5.Unit convertor\n");
+	printf("--------------------------------------------\n");
+	printf("1. Default calculator\n2.Engineering calculator\n3.Programmer calculator\n4.Date calculator\n5.Unit convertor\n");
+	printf("--------------------------------------------\n");
 	int menu;
 	scanf("%d",&menu);
 	if(menu==1)
@@ -13,6 +15,22 @@ void manual()
 		printf("This menu is Standard calculator. Perform basic arithmetic operations as well as square root and square operations\n");
 
 	}
+	else if(menu ==2)
+	{
+		printf("This menu is Engineering calculator. Perform factorials, modular calculations, and logarithmic operations that are deeper than regular calculators.\n");
+	}
+	else if(menu ==3)
+	{
+		printf("This menu is Programmer calculator. Perform logical operations such as OR,XOR,AND,NOT, for program implementation.\n");
+	}
+	else if(menu==4)
+	{
+		printf("This menu is Date calculator. Perform Date differences, D-day\n");
+	}
+	else if(menu==5)
+	{
+		printf("This menu is Unit convertor. Perform data conversion, and volume conversion.\n");
+	}	
 }
 
 
