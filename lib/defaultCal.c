bc43d4a62ca1aa37b261ee53f08b322869caf6ef
Kim Dong <dong7061@gmail.com>
2017-11-10 17:17:21 +0900

    operator

--- lib/defaultCal.c
+++ lib/defaultCal.c
@@ -50,3 +50,50 @@ int pop()
 	return temp;
   }
 }
+
+void print_stack()
+{
+	printf("Stack : Top --> Bottom \n");
+	for(int i=top; i>=0 ; i--)
+	{
+		printf("%d",stack[i]);
+	}
+}
+
+int get_stack_top()
+{
+  if(isEmpty() ==  1)
+  {
+	return -1;
+  }
+  return stack[top];
+}
+
+int is_operator(char k)
+{
+	return (k == '+' || k== '-' || k == '/' || k=='*');
+}
+
+int is_legal(char *s)
+{
+  int f= 0;
+  while(*s)
+  {
+	while(*s == ' '){
+		s++;
+        }
+	if(is_operator(*s)){
+		f--;
+	}
+	else{
+		f++;
+		while(*s != ' '){
+			s++;
+		}
+	}
+	if(f <1)break;
+	s++;
+  }
+  return (f==1);
+
+}
