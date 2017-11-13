c40dc65bbdde2432ef4383fbd81fe358967991ad
Kim Dong <dong7061@gmail.com>
2017-11-13 21:44:34 +0900

    defaultCal_1_K

--- lib/defaultCal.c
+++ lib/defaultCal.c
@@ -10,45 +10,49 @@ int stack[MAX_SIZE];
 
 int top = -1;
 
+void init_stack()
+{
+	top = -1;
+}
 int isEmpty()
 {
-  return (top < 0) ? 1 : 0;
+	return (top < 0) ? 1 : 0;
 }
 
 int isFull()
 {
-  return (top == (MAX_XIZE-1)) ? 1 : 0;
+	return (top == (MAX_SIZE-1)) ? 1 : 0;
 }
 
 int push(int t)
 {
-  if(isFull() == 1)
-  {
-	printf("Stack is overflow\n");
-	return -999;
-  }
-  else
-  {
- 	top++;
- 	stack[top] = t;
- 	return true;
-  }
+	if(isFull() == 1)
+	{
+		printf("Stack is overflow\n");
+		return -999;
+	}
+ 	else
+	{
+ 		top++;
+		stack[top] = t;
+		return true;
+	}
 }
 
 int pop()
 {
-  if(isEmpty() == 1)
-  {
-	printf("Stack is Empty\n");
-	return -999;
-  }
-  else
-  {
-	int temp=0;
-	temp = stack[top];
-	top--;
-	return temp;
-  }
+ 	if(isEmpty() == 1)
+	{
+		printf("Stack is Empty\n");
+		return -999;
+	}
+ 	else
+  	{
+		int temp=0;
+		temp = stack[top];
+		top--;
+		return temp;
+  	}
 }
 
 void print_stack()
@@ -62,11 +66,11 @@ void print_stack()
 
 int get_stack_top()
 {
-  if(isEmpty() ==  1)
-  {
-	return -1;
-  }
-  return stack[top];
+  	if(isEmpty() ==  1)
+  	{
+		return -1;
+  	}
+  	return stack[top];
 }
 
 int is_operator(char k)
@@ -76,24 +80,129 @@ int is_operator(char k)
 
 int is_legal(char *s)
 {
-  int f= 0;
-  while(*s)
-  {
-	while(*s == ' '){
-		s++;
-        }
-	if(is_operator(*s)){
-		f--;
-	}
-	else{
-		f++;
-		while(*s != ' '){
+	int f= 0;
+	while(*s)
+  	{
+		while(*s == ' '){
 			s++;
+        	}
+		if(is_operator(*s)){
+			f--;
+		}
+		else{
+			f++;
+			while(*s != ' '){
+				s++;
+			}
+		}
+		if(f <1)break;
+		s++;
+  	}
+	return (f==1);
+
+}
+int precedence(int op) {
+	if (op == '(') return 0;
+	if (op == '+' || op == '-') return 1;
+	if (op == '*' || op == '/') return 2;
+	else return 3;
+}
+void postfix(char *dst,char *src)
+{
+	char c;
+	init_stack();
+
+	while(*src)
+	{
+		if(*src == '('){
+			push(*src);
+			src++;
+		}else if (*src == ')') {
+			while (get_stack_top() != '(') {
+				*dst++ = pop();
+				*dst++ = ' ';
+			}
+			pop();
+			src++;
+		}
+		else if (is_operator(*src)) {
+			while (!isEmpty() &&
+				precedence(get_stack_top()) >= precedence(*src)) {
+				*dst++ = pop();
+				*dst++ = ' ';
+			}
+			push(*src);
+			src++;
+		}
+		else if (*src >= '0' && *src <= '9') {
+			do {
+				*dst++ = *src++;
+			} while (*src >= '0' && *src <= '9');
+			*dst++ = ' ';
+		}
+		else {
+			src++;
+		}
+  	}
+	while (!isEmpty()) {
+		*dst++ = pop();
+		*dst++ = ' ';
+	}
+	dst--;
+	*dst = 0;
+}
+
+int calc(char *p) {
+	int i;
+	init_stack();
+	while (*p) {
+		if (*p >= '0' && *p <= '9') {
+			i = 0;
+			do {
+				i = i * 10 + *p - '0';
+				p++;
+			} while (*p >= '0' && *p <= '9');
+			push(i);
+		}
+		else if (*p == '+') {
+			push(pop() + pop());
+			p++;
+		}
+		else if (*p == '*') {
+			push(pop() * pop());
+			p++;
+		}
+		else if (*p == '-') {
+			i = pop();
+			push(pop() - i);
+			p++;
+		}
+		else if (*p == '/') {
+			i = pop();
+			push(pop() / i);
+			p++;
+		}
+		else {
+			p++;
 		}
 	}
-	if(f <1)break;
-	s++;
-  }
-  return (f==1);
+	return pop();
+}
+int main(void) {
 
+	int r;
+	char exp[256] = "121*(0+1)/(2-4)";
+	char pf[256];
+
+	postfix(pf, exp);
+	printf("Postfix : %s\n", pf);
+
+	if (!is_legal(pf)) {
+		printf("Exprssion is legal!\n");
+		exit(1);
+	}
+	r = calc(pf);
+	printf("Answer : %d\n", r);
+
+	return 0;
 }
