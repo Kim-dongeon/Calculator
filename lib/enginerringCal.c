ef2fdb74ea2355237ea9d0b4dee4ea13901e3daf
Kim Dong <dong7061@gmail.com>
2017-11-13 22:15:09 +0900

    enginerringCal_0_tak

---
+++ lib/enginerringCal.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.1415926535897
+
+double getRadian(int _num)
+{
+	double radian;
+	radian = _num * (PI / 180);
+	return radian
+}
+
+double Sind(double _num)
+{
+
+}
+
+double Cosd(double _num)
+{
+
+}
+
+double Tand(double _num)
+{
+
+}
+
+double Sinh(double _num)
+{
+
+}
+double Tanh(double _num)
+{
+
+}
+double Cosh(double _num)
+{
+
+}
+double Sqrt(double _num)
+{
+
+}
+double Log(double _num)
+{
+
+}
+double LogT(double _num)
+{
+
+}
+double Exp(double _num)
+{
+
+}
+int main()
+{
+
+
+
+
+	return 0;
+}
