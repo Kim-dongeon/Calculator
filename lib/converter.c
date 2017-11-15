136ee0540bf287dd864c10570a3eae5cf821a2a8
TakHyunJoon <qlroxn@naver.com>
2017-11-15 23:23:03 +0900

    converter_0_t

---
+++ lib/converter.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void parsing()
+{
+
+}
+
+void kmTocm()
+{
+
+}
+
+void cmTokm()
+{
+
+}
+void cmTom()
+{
+
+}
+
+void mTocm()
+{
+
+}
+
+void inchTocm()
+{
+
+}
+
+void cmToinch()
+{
+
+}
+
+void mToinch()
+{
+
+}
+
+void inchTom()
+{
+
+}
+
+void kmToinch()
+{
+
+}
+
+void inchToKm()
+{
+
+}
+int main()
+{
+
+}
