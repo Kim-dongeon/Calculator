be595d55506c02a80f3182c530dbab3be89ee2c2
Kim Dong <dong7061@gmail.com>
2017-11-18 19:34:51 +0900

    defaultCal.h_k

--- include/defaultCal.h
+++ include/defaultCal.h
@@ -2,6 +2,17 @@
 #define DEFAULTCAL_H
 
 int mode_select();
+void init_stack();
+int isEmpty();
+int isFull();
+int push(int t);
+int pop();
+void print_stack();
+int is_operator(char k);
+int is_legal(char *s);
+int precedence(int op);
+void postfix(char *dst,char *src);
+int calc(char *p);
 
 #endif
 
