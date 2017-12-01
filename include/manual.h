9bdf767ed864b0a496481e071638e022c6bb213a
Kim Dong <dong7061@gmail.com>
2017-12-01 16:26:05 +0900

    manual.h_k

---
+++ include/manual.h
@@ -0,0 +1,10 @@
+#ifndef MANUAL_H
+#define MANUAL_H
+
+void showEngineeringCalManual();
+void showDefaultCalManual();
+void showProgrammerCalManual();
+void showConverterCalManual();
+void manual();
+
+#endif
