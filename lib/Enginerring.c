5b730609b30fa203fba850adf7eeaa50c7cd7138
TakHyunJoon <qlroxn@naver.com>
2017-12-04 23:02:01 -0800

    converter.c

---
+++ lib/Enginerring.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.1415926535897
+
+char func[20];
+char param[20];
+int degree;
+
+void parsing(char *string)
+{
+	strcpy(func, strtok(string, "("));
+	strcpy(param, strtok(NULL, ")"));
+
+}
+
+void isLogical(char *ch)
+{
+	parsing(ch);
+	printf("%s --> %s\n", func, param);
+}
+
+double getRadian(double _num)
+{
+	double radian;
+	radian = _num * (PI / 180);
+	return radian;
+}
+//////////////////////////////////////////////////////////
+double Sind(double _num)
+{
+
+	double rad = getRadian(_num);
+	double num = sin(rad);
+	return num;
+}
+double Cosd(double _num)
+{
+	double rad = getRadian(_num);
+	double num = cos(rad);
+	return num;
+}
+
+double Log(double _num)
+{
+}
+double LogT(double _num)
+{
+
+}
+double Tand(double _num)
+{
+	double rad = getRadian(_num);
+	double num = tan(rad);
+	return num;
+
+}
+double Sqrt(double _num)
+{
+
+}
+
+double Exp(double _num)
+{
+}
+double Sinh(double _num)
+{
+	double rad = getRadian(_num);
+	double num = asin(rad);
+	return num;
+
+}
+double Tanh(double _num)
+{
+	double rad = getRadian(_num);
+	double num = atan(rad);
+	return num;
+}
+double Cosh(double _num)
+{
+	double rad = getRadian(_num);
+	double num = acos(rad);
+	return num;
+}
+
+int main()
+{
+	char ch[256];
+	char ch1[256];
+	int degree1=0;
+	printf("Select function what you want\n");
+
+	printf("1. sin    2. cos    3. tan\n4. asin   5. acos   6. atan\n7. log    8. log10\n9. sqrt  10. exp\n");
+
+	gets(ch);
+	
+	if (strlen(ch) > 2)
+	{
+		isLogical(ch);
+		degree = atoi(param);
+	}
+
+	if (strcmp(func, "sin") == 0||strcmp(ch,"1")==0)
+	{
+		if (strcmp(ch, "1") == 0)
+		{
+			printf("Please fill in the angle you want\n");
+			gets(ch1);
+			degree1 = atoi(ch1);
+			printf("%f\n", Sind(degree1));
+
+		}
+		else {
+			printf("%f\n", Sind(degree));
+		}		
+	}
+	else if (strcmp(func, "cos") == 0 || strcmp(ch, "2") == 0)
+	{
+		
+		if (strcmp(ch, "2") == 0)
+		{
+			printf("Please fill in the angle you want\n");
+			gets(ch1);
+			degree1 = atoi(ch1);
+			printf("%f\n", Cosd(degree1));
+
+		}
+		else {
+			printf("%f\n", Cosd(degree));
+		}
+	}
+	else if (strcmp(func, "tan") == 0 || strcmp(ch, "3") == 0)
+	{
+		
+		if (strcmp(ch, "3") == 0)
+		{
+			printf("Please fill in the angle you want\n");
+			gets(ch1);
+			degree1 = atoi(ch1);
+			printf("%f\n", Tand(degree1));
+
+		}
+		else {
+			printf("%f\n", Tand(degree));
+		}
+	}
+	else if (strcmp(func, "asin") == 0 || strcmp(ch, "4") == 0)
+	{
+		if (strcmp(ch, "4") == 0)
+		{
+			printf("Please fill in the angle you want\n");
+			gets(ch1);
+			degree1 = atoi(ch1);
+			printf("%f\n", Sinh(degree1));
+
+		}
+		else {
+			printf("%f\n", Sinh(degree));
+		}
+	}
+	else if (strcmp(func, "acos") == 0 || strcmp(ch, "5") == 0)
+	{
+		
+		if (strcmp(ch, "5") == 0)
+		{
+			printf("Please fill in the angle you want\n");
+			gets(ch1);
+			degree1 = atoi(ch1);
+			printf("%f\n", Cosh(degree1));
+
+		}
+		else {
+			printf("%f\n", Cosh(degree));
+		}
+	}
+	else if (strcmp(func, "atan") == 0 || strcmp(ch, "6") == 0)
+	{
+		
+		if (strcmp(ch, "6") == 0)
+		{
+			printf("Please fill in the angle you want\n");
+			gets(ch1);
+			degree1 = atoi(ch1);
+			printf("%f\n", Tanh(degree1));
+
+		}
+		else {
+			printf("%f\n", Tanh(degree));
+		}
+	}
+	else if (strcmp(func, "log") == 0)
+	{
+		
+		printf("%f", log(degree));
+	}
+
+	
+	
+}
+
+
+
+
+
+
+
--- lib/converter.c
+++ lib/converter.c
@@ -1,62 +1,129 @@
 #include <stdio.h>
 
-void parsing()
-{
 
+float kmTocm(float data)
+{
+	double ret;
+	ret = data * 100000;
+	return ret;
 }
 
-void kmTocm()
+float cmTokm(float data)
 {
-
+	double ret;
+	ret = data / 100000;
+	return ret;
 }
 
-void cmTokm()
+float cmTom(float data)
 {
-
-}
-void cmTom()
-{
-
+	double ret;
+	ret = data / 100;
+	return ret;
 }
 
-void mTocm()
+float mTocm(float data)
 {
-
+	double ret;
+	ret = data * 100;
+	return ret;
 }
 
-void inchTocm()
+float inchTocm(float data)
 {
-
+	double ret;
+	ret = data * 2.54;
+	return ret;
 }
 
-void cmToinch()
+float cmToinch(float data)
 {
-
+	double ret;
+	ret = data / 2.54;
+	return ret;
 }
 
-void mToinch()
+float mToinch(float data)
 {
-
+	double ret;
+	ret = data / 0.0254;
+	return ret;
+}
+float inchTom(float data)
+{
+	double ret;
+	ret = data * 0.0254;
+	return ret;
 }
 
-void inchTom()
+float kmToinch(float data)
 {
-
+	double ret;
+	ret = data / 0.0000254;
+	return ret;
 }
 
-void kmToinch()
+float inchToKm(float data)
 {
-
+	double ret;
+	ret = data * 0.0000254;
+	return ret;
 }
 
-void inchToKm()
-{
-
-}
 int main()
 {
-
 	char command[256];
-	gets(command);
-	parsing(command);
+	int select = 0;
+	float value = 0;
+	printf("Please select the unit conversion you want and give the number\n");
+	printf("1. Km -> Cm	2. Cm -> Km	3. Cm -> M	4. M -> Cm	5. inch -> Cm\n6. Cm -> inch	7. M -> inch	8. inch -> M	9. Km -> inch	10. inch -> Km\n");
+	while (1)
+	{
+		scanf("%d", &select);
+		printf("Please specify the value you want to convert\n");
+		scanf("%f", &value);
+
+		if (select == 1)
+		{
+			printf("The converted value is %f\n", kmTocm(value));
+		}
+		else if (select == 2)
+		{
+			printf("The converted value is %f\n", cmTokm(value));
+		}
+		else if (select == 3)
+		{
+			printf("The converted value is %f\n", cmTom(value));
+		}
+		else if (select == 4)
+		{
+			printf("The converted value is %f\n", mTocm(value));
+		}
+		else if (select == 5)
+		{
+			printf("The converted value is %f\n", inchTocm(value));
+		}
+		else if (select == 6)
+		{
+			printf("The converted value is %f\n", cmToinch(value));
+		}
+		else if (select == 7)
+		{
+			printf("The converted value is %f\n", mToinch(value));
+		}
+		else if (select == 8)
+		{
+			printf("The converted value is %f\n", inchTom(value));
+		}
+		else if (select == 9)
+		{
+			printf("The converted value is %f\n", kmToinch(value));
+		}
+		else if (select == 10)
+		{
+			printf("The converted value is %f\n", inchToKm(value));
+		}
+		printf("Please select the unit conversion you want and give the number\n");
+	}
 }
+
